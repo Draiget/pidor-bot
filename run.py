1e09c15715b3e0499c2f9f1167251429a59ec6a7
Vlad <draiget@zontwelg.com>
2017-10-23 22:07:04 +0400

    Add some debug info (show if message belongs to group chat)

--- run.py
+++ run.py
@@ -10,10 +10,10 @@ dialog_engine = DialogEngine()
 telegram_api_key = os.environ.get('TELEGRAM_TOKEN_PBOT')
 bot = telebot.TeleBot(telegram_api_key)
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(func=lambda message: True, content_types=["text"])
 def handleMessage(message):
     try:
-        log('Handle message [chat_id=%d]: %s' % (message.chat.id, message.text))
+        log('Handle message [chat_id=%d, is_group=%d]: %s' % (message.chat.id, message.chat.type == "group", message.text))
         answer = dialog_engine.choose_answer(message)
         if answer:
             bot.send_message(message.chat.id, answer)
